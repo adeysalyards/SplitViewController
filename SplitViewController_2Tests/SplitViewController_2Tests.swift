3cd8017ca7353c749d4a40d2162f74461be8a484
c1Codepath <adey.salyards@capitalone.com>
2016-05-12 18:53:29 -0400

    Initial Commit

---
+++ SplitViewController_2Tests/SplitViewController_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SplitViewController_2Tests.swift
+//  SplitViewController_2Tests
+//
+//  Created by Salyards, Adey on 5/12/16.
+//  Copyright © 2016 Salyards, Adey. All rights reserved.
+//
+
+import XCTest
+@testable import SplitViewController_2
+
+class SplitViewController_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
